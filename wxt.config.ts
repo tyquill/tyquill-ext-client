efe50acc8885a98dde7ba05836f29659f60a49e2
qyinm <qusseun@gmail.com>
2025-10-13 01:01:58 +0900

    chore: update version to 1.4.1 in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   manifest: {
     name: 'Tyquill',
-    version: '1.4.0',
+    version: '1.4.1',
     description: '__MSG_extDescription__',
     default_locale: 'en',
     permissions: ['storage', 'tabs', 'activeTab', 'scripting', 'contextMenus', 'sidePanel'],
