376906125788622946aab70068cdcab26bcd589e
molla11 <hsjang635@naver.com>
2025-10-28 21:29:04 +0900

    chore: webNavigation 권한 추가

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     version: '1.4.3',
     description: '__MSG_extDescription__',
     default_locale: 'en',
-    permissions: ['storage', 'tabs', 'activeTab', 'scripting', 'contextMenus', 'sidePanel'],
+    permissions: ['storage', 'tabs', 'activeTab', 'scripting', 'contextMenus', 'sidePanel', 'webNavigation'],
     host_permissions: ['https://maily.so/*', '<all_urls>'],
     icons: {
       16: '/icon16.png',
